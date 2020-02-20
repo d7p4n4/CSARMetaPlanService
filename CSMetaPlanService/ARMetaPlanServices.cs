21a7b834e032169ab9b670aa2e49aef4ca16b65c
d7p4n4 <bercikezs199999999@gmail.com>
2020-02-20 11:12:56 +0100

    Add project files.

---
+++ CSMetaPlanService/ARMetaPlanServices.cs
@@ -0,0 +1,33 @@
+﻿using CSARMetaPlan.Class;
+using CSARMetaPlanDBCap;
+using Modul.Final.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMetaPlanService
+{
+    class ARMetaPlanServices
+    {
+
+        public MuveletResponse SaveMuvelet(MuveletRequest request)
+        {
+            MuveletResponse response = new MuveletResponse();
+
+            try
+            {
+                    new ARMetaPlanDBCapMethods().SaveMuvelet(request.Muvelet);
+
+                response.Result = new Ac4yProcessResult() { Code = Ac4yProcessResult.SUCCESS };
+            }
+            catch (Exception exception)
+            {
+                response.Result = (new Ac4yProcessResult() { Code = Ac4yProcessResult.FAIL, Message = exception.Message, Description = exception.StackTrace });
+            }
+            return response;
+        }
+
+    }
+}
---
+++ CSMetaPlanService/Program.cs
@@ -0,0 +1,26 @@
+﻿using CSARMetaPlan.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMetaPlanService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ARMetaPlanServices aRMetaPlanServices = new ARMetaPlanServices();
+
+            MuveletResponse muveletResponse =
+                aRMetaPlanServices.SaveMuvelet(new MuveletRequest()
+            {
+                Muvelet = new Muvelet()
+                {
+                    ValaszTipus = 7
+                }
+            });
+        }
+    }
+}
